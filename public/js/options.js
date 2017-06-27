8085287265e0335baf47f0533286242d4cc93d98
Kevin Genus <kevin.genus@gmail.com>
2017-06-27 14:36:06 -0400

    Post Lunch

--- public/js/options.js
+++ public/js/options.js
@@ -1,25 +1,32 @@
 $(document).ready(function () {
     let $hotelChoices = $('#hotel-choices'),
         $restaurantChoices = $('#restaurant-choices'),
-        $activityChoices = $('#activity-choices');
+        $activityChoices = $('#activity-choices'),
+        $optionsPanel = $('#options-panel');
+
 
     hotels.forEach(function (val) {
-        $hotelChoices.append(`<option>${val.name}</option>`);
+        $hotelChoices.append(`<option id="${val.id}">${val.name}</option>`);
     });
 
     restaurants.forEach(function (val) {
-        $restaurantChoices.append(`<option>${val.name}</option>`);
+        $restaurantChoices.append(`<option id="${val.id}">${val.name}</option>`);
     });
 
     activities.forEach(function (val) {
-        $activityChoices.append(`<option>${val.name}</option>`);
+        $activityChoices.append(`<option id="${val.id}">${val.name}</option>`);
     });
 
-    $('button').on('click', function() {
+    $optionsPanel.on('click', 'button[data-action=add]', function() {
         //let r = $('<input type="button" value="new button" />');
         // let removeButton = $(`<button class="btn btn-xs btn-danger remove btn-circle">x</button>-`);
         // fetch data
-        alert( $('[data-type]').text() );
+        // alert( $('[data-type]').text() );
+        // let select = $($(this)[0].previousElementSibling);
+        // let select = $('#hotel-choices');
+        // let select = $('#hotel-choices');
+        let $type = $(this).siblings('select').data('type');
+        let $id = $(this).siblings('select').find(':selected').val();
     });
 
 
